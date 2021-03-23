96c2a8606a0d6846b953b46ec2293249b34d4a85
kaution b <kaution.burrowes@gmail.com>
2021-03-23 20:48:01 +1300

    Delete python directory

--- python/problem_1.py
+++
@@ -1,9 +0,0 @@
-# Find the sum of multiples of 3 and 5 below 1000
-# https://projecteuler.net/problem=1
-MAX = 999
-n_sum = 0
-
-for n in range(MAX+1):
-	if not n % 3 or not n % 5: n_sum += n
-
-print(n_sum)
