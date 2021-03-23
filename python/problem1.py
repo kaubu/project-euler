cc76e9e558e6f8c95766163fc39494dfed1092f5
kaution b <kaution.burrowes@gmail.com>
2021-03-23 20:23:57 +1300

    Find the sum of multiples of 3 and 5 below 1000

--- python/problem1.py
+++ python/problem1.py
@@ -1,11 +1,10 @@
 # Find the sum of multiples of 3 and 5 below 1000
 # https://projecteuler.net/problem=1
 MIN = 1
-MAX = 1000
+MAX = 999
 n_sum = 0
 
-for n in range(MIN, MAX):
-	if not n % 3 or not n % 5:
-		n_sum += n
+for n in range(MIN, MAX+1):
+	if not n % 3 or not n % 5: n_sum += n
 
 print(n_sum)
