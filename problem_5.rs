14245af50cdf289c8f9a28c8e021beab9b75557b
kaution b <kaution.burrowes@gmail.com>
2021-03-23 21:40:40 +1300

    Find the smallest positive number that is evenly divisible by all of the numbers from 1 to 20

---
+++ problem_5.rs
@@ -0,0 +1,25 @@
+// Find the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+// https://projecteuler.net/problem=5
+// Time: 26.94s
+
+const MIN: i32 = 1;
+const MAX: i32 = 20;
+
+fn main() {
+	let mut counter = MIN;
+
+	loop {
+		for n in MIN..(MAX+1) {
+			if (counter % n) == 0 {
+				if n == MAX {
+					println!("{}", counter);
+					std::process::exit(0);
+				};
+			} else {
+				break;
+			}
+		}	
+
+		counter += 1;
+	}
+}
